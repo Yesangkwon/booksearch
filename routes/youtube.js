c71f99972ab5582277d36b7598868a198f896199
Yesangkwon <ye6350@gmail.com>
2025-05-22 18:06:08 +0900

    도서검색프로그램index

--- routes/youtube.js
+++ routes/youtube.js
@@ -5,5 +5,17 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+//버전1
+router.get('/step1', function(req, res, next){
+  res.render('index', { title: "유튜브-버전1", pageName: "page/youtube/step1.ejs", email: null })
+})
+//버전2
+router.get('/step2', function(req, res, next){
+  res.render('index', { title: "유튜브-버전2", pageName: "page/youtube/step1.ejs", email: null })
+})
+//버전3
+router.get('/step3', function(req, res, next){
+  res.render('index', { title: "유튜브-버전3", pageName: "page/youtube/step1.ejs", email: null })
+})
 
 module.exports = router;
